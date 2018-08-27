37c66191e9485a0eb33c4aefac818a59e57181e7
waylight3 <waylight3@naver.com>
2018-08-27 11:35:46 +0900

    add word2vec at prepro

--- seq2seq2/prepro.py
+++ seq2seq2/prepro.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import random, json, sys, os
+from nltk.tokenize import sent_tokenize, word_tokenize
 from util.util import pgbar
 
 if __name__ == '__main__':
@@ -12,6 +13,13 @@ if __name__ == '__main__':
 	data_train = []
 	data_dev = []
 	data_test = []
+
+	### for word2vec dict
+	word_top = 1000
+	word_dict = {}
+	word_cnt = {}
+
+	#################### prepro data ####################
 
 	print('[PREPRO] setting: train_ratio(%.2f) / dev_ratio(%.2f) / test_ratio(%.2f)' % (train_ratio, dev_ratio, 1.0 - train_ratio - dev_ratio))
 
@@ -51,3 +59,44 @@ if __name__ == '__main__':
 		for d in pgbar(data_test, pre='[test.data]'):
 			fp.write(json.dumps(d) + '\n')
 	print('[PREPRO] saving data finish')
+
+	#################### prepro word2vec ####################
+
+	print('[PREPRO] setting: word_top(%d)' % word_top)
+
+	### split into sentences/words and count all words
+	print('[PREPRO] counting words start')
+	for d in pgbar(data, pre='[data]'):
+		sents = sent_tokenize(d['text'].strip())
+		for sent in sents:
+			words = word_tokenize(sent.strip())
+			for word in words:
+				if not word in word_cnt:
+					word_cnt[word] = 0
+				word_cnt[word] += 1
+	print('[PREPRO] counting words finish')
+
+	### count words and find top words
+	total_word = len(word_cnt)
+	print('[PREPRO] total %d words find' % total_word)
+	word_set = set(sorted(word_cnt, key=lambda w:word_cnt[w], reverse=True)[:word_top])
+
+	word2vec_result = []
+
+	### read word2vec from glove
+	print('[PREPRO] matching used words and word2vec start')
+	with open('data/glove/glove.6B.100d.txt', 'r', encoding='utf-8') as fp:
+		word2vec_list = fp.read().strip().split('\n')
+		for word2vec in pgbar(word2vec_list, pre='glove.6B.100d.txt'):
+			word = word2vec.split()[0]
+			# use only used words
+			if not word in word_set: continue
+			word2vec_result.append(word2vec)
+	print('[PREPRO] matching used words and word2vec finish')
+
+	### save word2vec data
+	print('[PREPRO] saving word2vec start')
+	with open('seq2seq2/data/word2vec.data', 'w', encoding='utf-8') as fp:
+		for wv in pgbar(word2vec_result, pre='[word2vec.data]'):
+			fp.write(wv + '\n')
+	print('[PREPRO] saving word2vec finish')
